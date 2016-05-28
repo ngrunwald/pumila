4528b11106063f08f0ae426158768a0f9d5ec7b5
Nils <nils@grunwald.fr>
2016-05-28 16:35:19 +0200

    better ordonnanced tests

--- test/pumila/core_test.clj
+++ test/pumila/core_test.clj
@@ -10,28 +10,40 @@
   (Thread/sleep lag)
   true)
 
-(let [com (make-commander)]
-  (try
-    (expect pumila.core.Commander com)
-    (expect (more-of p
-                     true (deref p 100 nil)
-                     nil (:queue-duration (meta p))
-                     nil (:call-duration (meta p)))
-            (queue com (laggy-fn 10)))
-    (expect true (exec com (laggy-fn 10)))
-    (expect (more-of p
-                     true (deref p 100 nil)
-                     #(>= % 0) (deref (:queue-duration (meta p)) 100 nil)
-                     #(>= % 10) (deref (:call-duration (meta p)) 100 nil))
-            (queue com {:metric "mymet"} (laggy-fn 10)))
-    (let [tmout (promise)]
-      (expect (more-of p
-                       ;; :pumila.core/timeout (deref p)
-                       Exception (deref tmout))
-              (queue com {:timeout 10 :error-fn #(deliver tmout %)} (laggy-fn 1000))))
-    (let [tmout (promise)]
-      (expect (more-of p
-                       :fallback (deref p 100 nil)
-                       Exception (deref tmout 100 nil))
-              (queue com {:error-fn #(deliver tmout %) :fallback-fn (constantly :fallback)}
-                     ((fn [] (throw (ex-info "error" {})))))))))
+(defmacro with-com
+  [binding & body]
+  `(let [~(first binding) (make-commander ~(second binding))]
+     (try
+       ~@body
+       (finally
+         (close ~(first binding))))))
+
+(expect pumila.core.Commander (with-com [com {}] com))
+(expect (more-of p
+                 true (deref p 100 nil)
+                 nil (:queue-duration (meta p))
+                 nil (:call-duration (meta p)))
+        (with-com [com {}]
+          (queue com (laggy-fn 10))))
+(expect true (with-com [com  {}]
+               (exec com (laggy-fn 10))))
+(expect (more-of p
+                 true (deref p 100 nil)
+                 #(>= % 0) (deref (:queue-duration (meta p)) 100 nil)
+                 #(>= % 10) (deref (:call-duration (meta p)) 100 nil))
+        (with-com [com {}]
+          (queue com {:metric "mymet"} (laggy-fn 10))))
+(expect (more-of [p tmout]
+                 :pumila.core/timeout (deref p)
+                 Exception (deref tmout))
+        (with-com [com {}]
+          (let [tmout (promise)]
+            [(queue com {:timeout 10 :error-fn #(deliver tmout %)} (laggy-fn 1000)) tmout])))
+(expect (more-of [p tmout]
+                 :fallback (deref p 100 nil)
+                 Exception (deref tmout 100 nil))
+        (with-com [com {}]
+          (let [tmout (promise)]
+            [(queue com {:error-fn #(deliver tmout %) :fallback-fn (constantly :fallback)}
+                    ((fn [] (throw (ex-info "error" {})))))
+             tmout])))
