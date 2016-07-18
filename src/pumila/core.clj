6b20dfddedf5e9f7f760cc6aa9fbb79e9d0f155d
Nils Grunwald <nils@grunwald.fr>
2016-07-18 15:35:25 +0200

    better exception thrown by exec

--- src/pumila/core.clj
+++ src/pumila/core.clj
@@ -125,7 +125,7 @@
                                         (let [exi (ex-info "Error calling command"
                                                            {:commander (:label commander) :args args
                                                             :type :error :options options} e)]
-                                          (when-let [exp (::exp options)] (deliver exp e))
+                                          (when-let [exp (::exp options)] (deliver exp exi))
                                           (when metric
                                             (deliver queue-duration-atom (ms queue-latency))
                                             (met/mark!
