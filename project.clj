e054ad8e94637fa93b4dc9c97e129d2e9e63b82e
Nils <nils@grunwald.fr>
2016-08-03 12:13:03 +0200

    release v0.1.3

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject pumila "0.1.3-SNAPSHOT"
+(defproject pumila "0.1.3"
   :description "Lighter replacement for Netflix hystrix latency and fault tolerance library"
   :url "https://github.com/ngrunwald/pumila"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [io.aleph/dirigiste "0.1.2"]
-                 [metrics-clojure "2.6.1"]]
-  :profiles {:dev {:dependencies  [[expectations "2.1.8"]]
+                 [io.aleph/dirigiste "0.1.4"]
+                 [metrics-clojure "2.7.0"]]
+  :profiles {:dev {:dependencies  [[expectations "2.1.9"]]
                    :plugins [[lein-expectations "0.0.7"]]}})
