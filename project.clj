2f3813bd8a36fe47727d83752740606722604a03
Stéphane Raux <stephane.raux@oscaro.com>
2016-08-08 11:39:10 +0200

    Add diehard dependency

--- project.clj
+++ project.clj
@@ -5,6 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [io.aleph/dirigiste "0.1.4"]
+                 [diehard "0.2.2"]
                  [metrics-clojure "2.7.0"]]
   :profiles {:dev {:dependencies  [[expectations "2.1.9"]]
-                   :plugins [[lein-expectations "0.0.7"]]}})
+                   :plugins [[lein-expectations "0.0.7"]]}}
+  :repl-options {:init-ns pumila.core})  
