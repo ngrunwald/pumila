1e9b539e258e3752164db2c821f8cbbec89aca46
Stéphane Raux <stephane.raux@oscaro.com>
2016-11-03 15:21:58 +0100

    Bump diehard dependency

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [io.aleph/dirigiste "0.1.4"]
-                 [diehard "0.2.2"]
+                 [diehard "0.3.5"]
                  [metrics-clojure "2.7.0"]]
   :profiles {:dev {:dependencies  [[expectations "2.1.9"]]
                    :plugins [[lein-expectations "0.0.7"]]}}
