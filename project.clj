0b06b83515d75c51c8a95d887a161f998521803b
Jérôme Prudent <jerome.prudent.ext@oscaro.com>
2020-04-09 11:14:10 +0200

    Bump dependencies

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "https://github.com/ngrunwald/pumila"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [io.aleph/dirigiste "0.1.4"]
-                 [diehard "0.3.5"]
-                 [metrics-clojure "2.7.0"]]
-  :profiles {:dev {:dependencies  [[expectations "2.1.9"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [io.aleph/dirigiste "0.1.5"]
+                 [diehard "0.9.4"]
+                 [metrics-clojure "2.10.0"]]
+  :profiles {:dev {:dependencies  [[expectations "2.1.10"]]
                    :plugins [[lein-expectations "0.0.7"]]}}
   :global-vars  {*warn-on-reflection* true}
   :repl-options {:init-ns pumila.core})
